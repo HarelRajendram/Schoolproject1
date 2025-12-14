192069fa2ea0df75311020f105148a8b30eef6a9
HarelRajendram <2534297@edu.vaniercollege.qc.ca>
2025-12-14 00:24:39 -0500

    Add the entire Assignment class

--- src/main/java/org/Harel/Assignment.java
+++ src/main/java/org/Harel/Assignment.java
@@ -1,7 +1,12 @@
 package org.Harel;
 
-import java.util.ArrayList;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.Random;
+@Setter
+@Getter
 public class Assignment {
     private String assignmentId;
     private String assignmentName;
@@ -9,22 +14,57 @@ public class Assignment {
     private ArrayList<Integer> scores = new ArrayList<>();
     private static int nextId = 1;
 
+    private double assignmentAvg;
+
+    /** Calculates the average of the assignment score
+     * method to get the average score
+     */
     public void calcAssignmentAvg() {
-        double sum = 0;
-        double count = 0;
+        int sum = 0;
+        int count = 0;
         for (Integer score : scores) {
-            if (score == null) {
-                continue;
-            } else {
-                sum+= score;
+            if (score != null) {
+                sum += score;
                 count++;
             }
         }
-        double assignAvg =(sum / count) * 100;
-        System.out.println(assignAvg);
+        if (count == 0) {
+            assignmentAvg = 0.0;
+        } else {
+            assignmentAvg = sum / (double) count;
+        }
     }
+    /** generates random scores for all students in an assignment
+     * each random score has a range to place the students
+     */
     public void generateRandomScore() {
+        Random rand = new Random();
+        for (int i = 0; i < scores.size(); i++) {
+            int p = rand.nextInt(0,11);
+            int score;
+            switch (p) {
+                case 0 -> score = rand.nextInt(0,60);
+                case 1, 2 -> score = rand.nextInt(60,70);
+                case 3, 4 -> score = rand.nextInt(70,80);
+                case 5, 6, 7, 8 -> score = rand.nextInt(80,90);
+                case 9, 10 -> score = 90 + rand.nextInt(11);
+                default -> score = 0;
+            }
+            scores.set(i ,score);
+        }
+    }
+    public Assignment (String assignmentName, double weight) {
+        this.assignmentId = String.valueOf(nextId++);
+        this.assignmentName = assignmentName;
+        this.weight = weight;
 
-
+    }
+    @Override
+    public String toString() {
+        return "Assignment{" +
+                "assignmentId='" + assignmentId + '\'' +
+                ", assignmentName='" + assignmentName + '\'' +
+                ", weight=" + weight +
+                '}';
     }
 }
