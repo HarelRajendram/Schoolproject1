5f072ac6425ec3fc0a7a7845b6244229b1c2585e
HarelRajendram <2534297@edu.vaniercollege.qc.ca>
2025-12-14 19:30:24 -0500

    Add the Course class

--- src/main/java/org/Harel/Course.java
+++ src/main/java/org/Harel/Course.java
@@ -1,8 +1,15 @@
 package org.Harel;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.Random;
 
+@EqualsAndHashCode
+@Getter
+@Setter
 public class Course {
     private String courseId;
     private String courseName;
@@ -13,8 +20,8 @@ public class Course {
     private static int nextId = 1;
 
     /**
-     *
-     * @return
+     *checks if the sum of weights of all assignments of that course equals to 100%
+     * @return a boolean value if sum is equal to 100
      */
     public boolean isAssignmentWeightValid() {
         int sum = 0;
@@ -25,7 +32,7 @@ public class Course {
     }
 
     /**
-     *
+     *adds a student to the student list of the course
      * @param student
      * @return
      */
@@ -47,7 +54,7 @@ public class Course {
     public int[] calcStudentsAverage() {
         int numStudent = registeredStudents.size();
         int[] averages = new int[numStudent];
-        for (int i = 0; i <= numStudent; i++) {
+        for (int i = 0; i < numStudent; i++) {
             double total = 0;
             for (Assignment a : assignment) {
                 Integer score = a.getScores().get(i);
@@ -76,7 +83,7 @@ public class Course {
             a.getScores().clear();
 
             for (int i = 0; i < registeredStudents.size();i++) {
-                int score = rand.nextInt(0, 100);
+                int score = rand.nextInt(0, 100 + 1);
                 a.getScores().add(score);
             }
         }
@@ -94,5 +101,76 @@ public class Course {
     }
     public void displayScores() {
         System.out.println("Course:" + courseName + courseId);
+        System.out.printf("%-20s", "");
+        for (Assignment a : assignment) {
+            System.out.printf("%-15s", a.getAssignmentName());
+        }
+
+        System.out.printf("%-15s%n", "FinalS core");
+
+        for (int i = 0; i< registeredStudents.size(); i++) {
+            Student student = registeredStudents.get(i);
+
+            System.out.printf("%-20s", student.getStudentName());
+
+            for (Assignment a : assignment) {
+                Integer score = a.getScores().get(i);
+                System.out.printf("%-15s", score == null ? "-" : score.toString());
+            }
+            System.out.printf("");
+        }
+
+        System.out.printf("%-20s", "Average");
+        for (Assignment a : assignment) {
+            int totalScore = 0;
+            int numAssignments = 0;
+
+            for (Integer score : a.getScores()) {
+                if (score != null) {
+                    totalScore += score;
+                    numAssignments ++;
+                }
+            }
+            int avg = numAssignments == 0 ? 0 : Math.round(totalScore / numAssignments);
+            System.out.printf("%-15d", avg);
+        }
+        System.out.println();
+    }
+    public String toSimplifiedString() {
+        return courseId + " " + courseName + " " + credits + " (" + department.getDepartmentName() + ")";
+    }
+    @Override
+    public String toString() {
+        String result = "Course ID: " + courseId +
+                ", Name: " + courseName +
+                ", Credits: " + credits +
+                ", Department: " + department.getDepartmentName() +
+                "\n";
+
+        result += "Assignments: ";
+        for (Assignment a : assignment) {
+            result += a.getAssignmentName() + " (" + a.getWeight() + "%), ";
+        }
+        result += "\n";
+
+        result += "Students: ";
+        for (Student student : registeredStudents) {
+            result += student.getStudentId() + " - " + student.getStudentName() +
+                    " (" + student.getDepartment().getDepartmentName() + "), ";
+        }
+        result += "\n";
+
+        result += "Assignment weights valid: " + isAssignmentWeightValid();
+
+        return result;
+    }
+
+    public Course(String courseId, String courseName, double credits, Department department, ArrayList<Assignment> assignment, ArrayList<Student> registeredStudents) {
+        this.courseId = String.format("C-%s-%02d", department.getDepartmentId(), nextId++);
+        this.courseName = courseName;
+        this.credits = credits;
+        this.department = department;
+        this.assignment = assignment;
+        this.registeredStudents = registeredStudents;
     }
 }
